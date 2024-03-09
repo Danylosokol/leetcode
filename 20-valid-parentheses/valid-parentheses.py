f6c0b5b066cfdfa6be8111a402fcb0d86c00cb86
Danylo Sokol <dansokol677@gmail.com>
2024-03-09 21:01:40 +0000

    Time: 38 ms (40.02%) | Memory: 16.7 MB (36.46%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        valid_pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']',
+        }
+        history = []
+        for item in s:
+            if item in valid_pairs:
+               history.append(item)
+            elif len(history):
+                last_in_history = history.pop()
+                if item != valid_pairs[last_in_history]:
+                    return False
+            else:
+                return False
+        if len(history):
+            return False
+        else:
+            return True
+        
