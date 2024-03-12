0ebc7498898be7cd1ae48de45470ad8403924a10
Danylo Sokol <dansokol677@gmail.com>
2024-03-12 08:27:50 +0000

    Time: 36 ms (67.67%) | Memory: 17.6 MB (98.22%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,7 +6,6 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
-            print("in a base case")
             return None
         
         newHead = head
