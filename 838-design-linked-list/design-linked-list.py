91d8ad7c8049d83005f7a28995e5c1f0adc96c32
Danylo Sokol <dansokol677@gmail.com>
2024-03-13 18:25:11 +0000

    Time: 107 ms (71.83%) | Memory: 18 MB (35.94%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -14,11 +14,11 @@ class MyLinkedList:
 
     def get(self, index: int) -> int:
         curr = self.left.next
-        while curr:
-            if index == 0:
-                return curr.val
+        while curr and index > 0:
             curr = curr.next
             index -= 1
+        if curr and curr != self.right and index == 0:
+            return curr.val
         return -1
 
     def addAtHead(self, val: int) -> None:
@@ -40,15 +40,15 @@ class MyLinkedList:
         new_node.next = next
 
     def addAtIndex(self, index: int, val: int) -> None:
-        curr = self.left
+        curr = self.left.next
 
-        while curr.next and index != 0:
+        while curr and index > 0:
             curr = curr.next
             index -= 1
         
-        if curr.next and index == 0:
-            prev = curr
-            next = curr.next
+        if curr and index == 0:
+            next = curr
+            prev = curr.prev
             new_node = ListNode(val)
             prev.next = new_node
             new_node.prev = prev
@@ -56,15 +56,15 @@ class MyLinkedList:
             new_node.next = next
 
     def deleteAtIndex(self, index: int) -> None:
-        curr = self.left
+        curr = self.left.next
 
-        while curr.next != self.right and index != 0:
+        while curr and index > 0:
             curr = curr.next
             index -= 1
         
-        if curr.next != self.right and index == 0:
-            prev = curr
-            next = curr.next.next
+        if curr and curr != self.right and index == 0:
+            prev = curr.prev
+            next = curr.next
             prev.next = next
             next.prev = prev
 
