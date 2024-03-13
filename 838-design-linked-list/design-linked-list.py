c102671181fc12827c79fb55068cd1532babeb4e
Danylo Sokol <dansokol677@gmail.com>
2024-03-13 08:04:19 +0000

    Time: 111 ms (64.34%) | Memory: 17.4 MB (65.18%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -15,7 +15,6 @@ class MyLinkedList:
         i = 0
         while curr:
             if i == index:
-                print(f"at index: {index} value: {curr.val}")
                 return curr.val
             curr = curr.next
             i += 1
@@ -27,15 +26,12 @@ class MyLinkedList:
         new_node.next = self.head.next
         self.head.next = new_node
         if new_node.next == None:
-            print("updating tail...")
             self.tail = new_node
 
     def addAtTail(self, val: int) -> None:
         new_node = ListNode(val)
         self.tail.next = new_node
         self.tail = new_node
-        # print("new tail:")
-        # print(self.tail.val)
         
 
     def addAtIndex(self, index: int, val: int) -> None:
@@ -48,33 +44,19 @@ class MyLinkedList:
         if curr:
             new_node.next = curr.next
             curr.next = new_node
-            print("adding new node:")
-            print(new_node.val)
-            print("new node pointer:")
-            print(new_node.next)
             if new_node.next == None:
-                print("updating tail...")
                 self.tail = new_node
 
     def deleteAtIndex(self, index: int) -> None:
-        print(f"deleting at index: {index}")
         curr = self.head
         i = 0
         while i < index and curr.next:
-            print("curr value in a loop:")
-            print(curr.val)
             curr = curr.next
             i += 1
         if curr.next and i == index:
-            # print("current value:")
-            # print(curr.val)
-            # print("next curr value:")
-            # print(curr.next.val)
             curr.next = curr.next.next
             if curr.next == None:
                 self.tail = curr
-            # print("next curr value after update:")
-            # print(curr.next.val)
 
 
 # Your MyLinkedList object will be instantiated and called as such:
