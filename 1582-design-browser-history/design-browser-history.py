eff57e3bd9e915ac4570483741dc33f5bdac74c4
Danylo Sokol <dansokol677@gmail.com>
2024-03-14 09:03:59 +0000

    Time: 238 ms (5.21%) | Memory: 19 MB (86.34%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,29 +1,31 @@
-class ListNode():
-    def __init__(self, val, next=None, prev=None):
-        self.val = val
-        self.next = next
-        self.prev = prev
-
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.curr = ListNode(homepage)
+        self.curr = 0
+        self.len = 1
+        self.history = [homepage]
 
     def visit(self, url: str) -> None:
-        self.curr.next = ListNode(url, None, self.curr)
-        self.curr = self.curr.next
+        if len(self.history) < self.curr + 2:
+            self.history.append(url)
+        else:
+            self.history[self.curr + 1] = url
+        self.curr += 1
+        self.len = self.curr + 1
 
     def back(self, steps: int) -> str:
-        while self.curr.prev and steps > 0:
-            self.curr = self.curr.prev
-            steps -= 1
-        return self.curr.val
+        self.curr = max(self.curr - steps, 0)
+        return self.history[self.curr]
 
     def forward(self, steps: int) -> str:
-        while self.curr.next and steps > 0:
-            self.curr = self.curr.next
-            steps -= 1
-        return self.curr.val
+        print("in forward:")
+        print(self.curr)
+        print(steps)
+        print(self.curr + steps)
+        print(self.len)
+        print(self.history)
+        self.curr = min(self.curr + steps, self.len - 1)
+        return self.history[self.curr]
 
 
 # Your BrowserHistory object will be instantiated and called as such:
