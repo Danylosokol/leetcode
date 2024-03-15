a195aec0076dd4cf1b867560a18a7184e57d5508
Danylo Sokol <dansokol677@gmail.com>
2024-03-15 08:51:01 +0000

    Time: 59 ms (5.47%) | Memory: 16.5 MB (95.53%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,14 +1,9 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        print(students)
-        print(sandwiches)
         result = 0
-        sandwichesCounter = {}
         studentsCounter = {}
         for i in range(len(students)):
             student = students[i]
-            sandwich = sandwiches[i]
-            sandwichesCounter[sandwich] = sandwichesCounter.get(sandwich, 0) + 1
             studentsCounter[student] = studentsCounter.get(student, 0) + 1
         print("I after while:")
         if not sandwiches:
@@ -21,9 +16,7 @@ class Solution:
             sandwiches.pop(0)
             return self.countStudents(students, sandwiches)
         else:
-            print("first element")
             removed_student = students.pop(0)
-            print(removed_student)
             students.append(removed_student)
             return self.countStudents(students, sandwiches)
         
