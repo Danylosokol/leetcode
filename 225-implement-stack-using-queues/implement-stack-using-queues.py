7f6c8673f89fd5487abe95410cd2dabf2d8c1ee4
Danylo Sokol <dansokol677@gmail.com>
2024-03-15 16:24:18 +0000

    Time: 34 ms (64.38%) | Memory: 16.5 MB (75.58%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque([])
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        temp_queue = deque([])
+        while len(self.queue) > 1:
+            temp_queue.append(self.queue.popleft())
+        result = self.queue.popleft()
+        while len(temp_queue):
+            self.queue.append(temp_queue.popleft())
+        return result
+
+    def top(self) -> int:
+        temp_queue = deque([])
+        print("in top:")
+        print(self.queue)
+        while len(self.queue) > 1:
+            temp_queue.append(self.queue.popleft())
+            print("temp_queue in while loop:")
+            print(temp_queue)
+        result = self.queue.popleft()
+        temp_queue.append(result)
+        while len(temp_queue):
+            self.queue.append(temp_queue.popleft())
+        print("end of top:")
+        print(self.queue)
+        return result
+
+    def empty(self) -> bool:
+        if len(self.queue):
+            return False
+        return True
+            
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
