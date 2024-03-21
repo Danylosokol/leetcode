4aa999858829e7633ad706834e77dff98d80613b
Danylo Sokol <dansokol677@gmail.com>
2024-03-21 19:20:51 +0000

    Time: 1164 ms (45.83%) | Memory: 24.1 MB (62.66%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -2,14 +2,45 @@ import random
 
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        if len(nums) <= 1: return nums
-
-        pivot = random.choice(nums)
-        less_than, equal_to, more_than = [], [], []
-
-        for val in nums:
-            if val < pivot: less_than.append(val)
-            elif val > pivot: more_than.append(val)
-            else: equal_to.append(val)
+        return self.mergeSort(nums, 0, len(nums) - 1)
+    
+    def mergeSort(self, arr, s, e):
+        if e - s + 1 <= 1:
+            return arr
             
-        return self.sortArray(less_than) + equal_to + self.sortArray(more_than)
+        m = (e + s)//2
+
+        self.mergeSort(arr, s, m)
+        self.mergeSort(arr, m+1, e)
+
+        return self.merge(arr, s, m, e)
+    
+    def merge(self, arr, s, m, e):
+        left_part = arr[s:m+1]
+        right_part = arr[m+1:e+1]
+
+        l = 0
+        r = 0
+        i = s
+        
+        while l < len(left_part) and r < len(right_part):
+            if left_part[l] <= right_part[r]:
+                arr[i] = left_part[l]
+                l += 1
+            else: 
+                arr[i] = right_part[r]
+                r += 1
+            i += 1
+
+        while l < len(left_part):
+            arr[i] = left_part[l]
+            i += 1
+            l += 1
+        while r < len(right_part):
+            arr[i] = right_part[r]
+            i += 1
+            r += 1
+
+        return arr
+
+            
