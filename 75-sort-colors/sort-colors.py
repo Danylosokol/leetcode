fd894e328fa5a64335361f36529122934569be48
Danylo Sokol <dansokol677@gmail.com>
2024-03-22 08:42:50 +0000

    Time: 32 ms (85.66%) | Memory: 16.6 MB (43.13%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,15 +1,23 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        counts = [0, 0, 0]
+        pivot_r = 2
+        pivot_l = 0
 
-        for i in nums:
-            counts[i] += 1
-        
-        idx = 0
-        for i in range(len(counts)):
-            for _ in range(counts[i]):
-                nums[idx] = i
-                idx += 1
-        
+        l = 0
+        r = len(nums) - 1
+        i = 0
+
+        while i < len(nums) and i <= r:
+            if nums[i] == pivot_l:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == pivot_r:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
+            
+            
     
         
