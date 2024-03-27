a83de722077803b4646ef6fdea4b2857abf57d5d
Danylo Sokol <dansokol677@gmail.com>
2024-03-27 19:02:12 +0000

    Time: 122 ms (56.49%) | Memory: 88.2 MB (21.80%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -15,5 +15,5 @@ class Solution:
 
         root.left = self.buildTree(preorder[1:i+1], inorder[:i])
         root.right = self.buildTree(preorder[i+1:], inorder[i+1:])
-        
+
         return root
