7ac6070d4e34e00b0ed7eb6afff61a551832cd00
Danylo Sokol <dansokol677@gmail.com>
2024-03-30 10:56:58 +0000

    Time: 30 ms (92.79%) | Memory: 16.8 MB (8.33%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,9 +3,7 @@ class Solution:
         if not nums: return [[]]
 
         num = nums[0]
-
         subsets_without_num = self.subsets(nums[1:])
         subsets_with_num = [[num] + subset for subset in subsets_without_num]
-        
-        return subsets_with_num + subsets_without_num
-        
+
+        return subsets_without_num + subsets_with_num
