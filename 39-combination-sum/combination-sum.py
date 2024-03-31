b9a036d67d7acbbd4ecf4078246d7d134c813226
Danylo Sokol <dansokol677@gmail.com>
2024-03-31 14:52:39 +0100

    Time: 76 ms (13.08%) | Memory: 16.7 MB (43.94%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -5,17 +5,11 @@ class Solution:
         def backtracking(indx, subarray):
             if indx >= len(candidates) or sum(subarray) > target:
                 return
-            print("--------")
-            print(subarray)
             val = candidates[indx]
             subarray.append(val)
 
             if sum(subarray) == target:
-                print("adding to result:")
-                print(subarray)
                 result.append(subarray)
-            print(subarray)
-            print("going to recursion")
             
             backtracking(indx, subarray[:])
             backtracking(indx + 1, subarray[:-1])
