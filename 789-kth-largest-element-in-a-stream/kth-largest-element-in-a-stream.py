fb785e53981cc4a80230238049b1d247d5e54dee
Danylo Sokol <dansokol677@gmail.com>
2024-04-03 07:51:08 +0100

    Time: 109 ms (13.26%) | Memory: 20.5 MB (68.16%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -3,53 +3,40 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.heap = [0]
         self.k = k
+
         for val in nums:
             self.add(val)
-        print("finish adding")
-
 
     def add(self, val: int) -> int:
         self.heap.append(val)
         i = len(self.heap) - 1
         while i > 1:
-            parent_idx = i // 2
-            if self.heap[parent_idx] > self.heap[i]:
-                self.heap[i], self.heap[parent_idx] = self.heap[parent_idx], self.heap[i]
-                i = parent_idx
+            parent_i = i//2
+            if self.heap[parent_i] > self.heap[i]:
+                self.heap[parent_i], self.heap[i] = self.heap[i], self.heap[parent_i]
+                i = parent_i
             else:
                 break
+        
         while len(self.heap) > self.k + 1:
-            self.pop(self.heap)
-        # print("heap after adding:")
-        # print(val)
-        # print(self.heap)
+            self.pop()
+        
         return self.heap[1]
     
-    def pop(self, heap):
-        if len(heap) <= 1:
-            return None
-        
-        if len(heap) == 2:
-            return heap.pop()
-        
-        res = heap[1]
-        heap[1] = heap[-1]
-        heap.pop()
-        idx = 1
-        while 2*idx < len(heap):
-            if 2*idx < len(heap) - 1 and heap[2*idx + 1] < heap[2*idx] and heap[2*idx + 1] <  heap[idx]:
-                heap[idx], heap[2*idx + 1] = heap[2*idx + 1], heap[idx]
-                idx = 2*idx + 1
-            elif heap[2*idx] < heap[idx]:
-                heap[idx], heap[2*idx] = heap[2*idx], heap[idx]
-                idx = 2*idx
+    def pop(self):
+        res = self.heap[1]
+        self.heap[1] = self.heap[-1]
+        self.heap.pop()
+        i = 1
+        while 2*i < len(self.heap):
+            if 2*i + 1 < len(self.heap) and self.heap[2*i + 1] < self.heap[2*i] and self.heap[2*i + 1] < self.heap[i]:
+                self.heap[2*i + 1], self.heap[i] = self.heap[i], self.heap[2*i + 1]
+                i = 2*i + 1
+            elif self.heap[2*i] < self.heap[i]:
+                self.heap[2*i], self.heap[i] = self.heap[i], self.heap[2*i]
+                i = 2*i
             else:
                 break
-        
-        return res
-
-            
-
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
