709a80365b79490ed025828b7a66db5d6bb5292c
Danylo Sokol <dansokol677@gmail.com>
2024-04-03 08:08:04 +0100

    Time: 69 ms (95.99%) | Memory: 20.4 MB (68.16%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,42 +1,20 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = [0]
+        self.min_heap = nums
         self.k = k
 
-        for val in nums:
-            self.add(val)
+        heapq.heapify(self.min_heap)
+        
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
-        self.heap.append(val)
-        i = len(self.heap) - 1
-        while i > 1:
-            parent_i = i//2
-            if self.heap[parent_i] > self.heap[i]:
-                self.heap[parent_i], self.heap[i] = self.heap[i], self.heap[parent_i]
-                i = parent_i
-            else:
-                break
-        
-        while len(self.heap) > self.k + 1:
-            self.pop()
-        
-        return self.heap[1]
-    
-    def pop(self):
-        res = self.heap[1]
-        self.heap[1] = self.heap[-1]
-        self.heap.pop()
-        i = 1
-        while 2*i < len(self.heap):
-            if 2*i + 1 < len(self.heap) and self.heap[2*i + 1] < self.heap[2*i] and self.heap[2*i + 1] < self.heap[i]:
-                self.heap[2*i + 1], self.heap[i] = self.heap[i], self.heap[2*i + 1]
-                i = 2*i + 1
-            elif self.heap[2*i] < self.heap[i]:
-                self.heap[2*i], self.heap[i] = self.heap[i], self.heap[2*i]
-                i = 2*i
-            else:
-                break
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
+
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
