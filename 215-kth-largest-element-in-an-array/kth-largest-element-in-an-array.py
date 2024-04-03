a400800813dc58e9d9f5930fd8ce1c1196dc0ec7
Danylo Sokol <dansokol677@gmail.com>
2024-04-03 19:41:00 +0100

    Time: 465 ms (64.74%) | Memory: 29.6 MB (85.11%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,45 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        self.mergeSort(nums, 0, len(nums) - 1)
-        return nums[-k]
+        min_heap = [-num for num in nums]
+        heapq.heapify(min_heap)
 
-    def mergeSort(self, nums, s, e):
-        if e - s + 1 <= 1:
-            return nums
+        while k > 1:
+            heapq.heappop(min_heap)
+            k -= 1
         
-        m = (e + s)//2
-
-        self.mergeSort(nums, s, m)
-        self.mergeSort(nums, m + 1, e)
-
-        return self.merge(nums, s, m, e)
-
-    def merge(self, nums, s, m, e):
-        left_part = nums[s:m+1]
-        right_part = nums[m+1:e+1]
-
-        l = 0
-        r = 0
-        k = s
-
-        while l < len(left_part) and r < len(right_part):
-            if left_part[l] <= right_part[r]:
-                nums[k] = left_part[l]
-                l += 1
-            else:
-                nums[k] = right_part[r]
-                r += 1
-            k += 1
-
-        while l < len(left_part):
-            nums[k] = left_part[l]
-            k += 1
-            l += 1
-        while r < len(right_part):
-            nums[k] = right_part[r]
-            k += 1
-            r += 1
-        
-        return nums
-
-    
+        return heapq.heappop(min_heap)*-1
