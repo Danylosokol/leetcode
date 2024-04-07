ca683d29410652ef2aeeb4598c85329f9e489987
Danylo Sokol <dansokol677@gmail.com>
2024-04-07 12:03:24 +0100

    Time: 65 ms (47.29%) | Memory: 18 MB (6.41%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         reminders = {}
 
-        for i, val in enumerate(nums):
+        for indx, val in enumerate(nums):
             if val not in reminders:
-                reminders[target - val] = i
+                reminders[target - val] = indx
             else:
-                return [reminders[val], i]
+                return [reminders[val], indx]
