23d99af26d9fe7b2e426d9aa1ef558e645c6d161
Danylo Sokol <dansokol677@gmail.com>
2024-04-07 12:01:00 +0100

    Time: 586 ms (40.99%) | Memory: 78.1 MB (22.26%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,6 +1,7 @@
 class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
         self.next = self.prev = None
 
 class LRUCache:
@@ -11,29 +12,28 @@ class LRUCache:
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
 
+    def insert(self, node):
+        prev, next = self.right.prev, self.right
+        prev.next = next.prev = node
+        node.next, node.prev = next, prev
+    
     def remove(self, node):
         prev, next = node.prev, node.next
         prev.next, next.prev = next, prev
-    
-    def insert_right(self, node):
-        prev, next = self.right.prev, self.right
-        prev.next = next.prev = node
-        node.prev, node.next = prev, next
 
     def get(self, key: int) -> int:
         if key in self.cash:
             self.remove(self.cash[key])
-            self.insert_right(self.cash[key])
-            return self.cash[key].val
+            self.insert(self.cash[key])
+            return self.cash[key].value
         else:
-            return - 1
+            return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cash:
             self.remove(self.cash[key])
         self.cash[key] = Node(key, value)
-        self.insert_right(self.cash[key])
-
+        self.insert(self.cash[key])
         if len(self.cash) > self.capacity:
             lru = self.left.next
             self.remove(lru)
