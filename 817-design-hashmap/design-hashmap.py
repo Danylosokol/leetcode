e50f4cb9ad4d18cfb63bb004a72d7d2b927642fc
Danylo Sokol <dansokol677@gmail.com>
2024-04-09 18:27:43 +0100

    Time: 276 ms (20.50%) | Memory: 21.9 MB (24.08%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -10,41 +10,32 @@ class MyHashMap:
         self.hashmap = [ListNode(0, 0) for i in range(10**4)]
 
     def put(self, key: int, value: int) -> None:
-        index = key % len(self.hashmap)
-
+        index = key%len(self.hashmap)
         curr = self.hashmap[index]
-
         while curr.next:
             if curr.next.key == key:
                 curr.next.val = value
                 return
             curr = curr.next
-        
         curr.next = ListNode(key, value)
-        
 
     def get(self, key: int) -> int:
-        index = key % len(self.hashmap)
-
+        index = key%len(self.hashmap)
         curr = self.hashmap[index]
-
         while curr.next:
             if curr.next.key == key:
                 return curr.next.val
             curr = curr.next
-        
         return -1
 
     def remove(self, key: int) -> None:
-        index = key % len(self.hashmap)
+        index = key%len(self.hashmap)
         curr = self.hashmap[index]
         while curr.next:
             if curr.next.key == key:
                 curr.next = curr.next.next
                 return
             curr = curr.next
-        
-
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
