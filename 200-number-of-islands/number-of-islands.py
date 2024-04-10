6b04da7ddd301010a3e08149eba0c6171cec40db
Danylo Sokol <dansokol677@gmail.com>
2024-04-10 19:58:59 +0100

    Time: 265 ms (37.86%) | Memory: 27.9 MB (9.29%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,19 +5,18 @@ class Solution:
         numOfRow = len(grid)
         numOfColls = len(grid[0])
 
-        def dfs(visited, r, c):
+        def dfs(r, c):
             if min(r, c) < 0 or r == numOfRow or c == numOfColls or (r, c) in ilandSpots or grid[r][c] == "0":
-                # ilandSpots.update(visited)
                 return
             ilandSpots.add((r, c))
-            dfs(visited, r+1, c)
-            dfs(visited, r-1, c)
-            dfs(visited, r, c+1)
-            dfs(visited, r, c-1)
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
             
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if (i, j) not in ilandSpots and grid[i][j] == "1":
-                    dfs(set(), i, j)
+                    dfs(i, j)
                     counter += 1
         return counter
