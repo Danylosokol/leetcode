e493167fe69adb7c9ceceb2c1f3e3807d27b0d8d
Danylo Sokol <dansokol677@gmail.com>
2024-04-11 08:04:52 +0100

    Time: 317 ms (12.15%) | Memory: 24.4 MB (18.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,22 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        counter = 0
+        if not grid:
+            return 0
         visited = set()
-        numOfRow = len(grid)
-        numOfColls = len(grid[0])
+        islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
 
-        def dfs(r, c):
-            if min(r, c) < 0 or r == numOfRow or c == numOfColls or (r, c) in visited or grid[r][c] == "0":
-                return
+        def bfs(r, c):
+            queue = deque()
+            queue.append((r, c))
             visited.add((r, c))
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-            
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if (i, j) not in visited and grid[i][j] == "1":
-                    dfs(i, j)
-                    counter += 1
-        return counter
+
+            while queue:
+                row, col = queue.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    nr = row + dr
+                    nc = col + dc
+                    if (nr) in range(rows) and (nc) in range(cols) and (nr, nc) not in visited and grid[nr][nc] == "1":
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        
+        return islands
+        
