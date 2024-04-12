a88c78e369f40854f6efee4857679d2a0f8a5705
Danylo Sokol <dansokol677@gmail.com>
2024-04-12 10:58:17 +0100

    Time: 131 ms (12.21%) | Memory: 17 MB (81.41%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,29 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        
+        max_size = 0
         visited = set()
         rows = len(grid)
         cols = len(grid[0])
-        max_size = 0
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        def dfs(r, c):
-            if (r) not in range(rows) or (c) not in range(cols) or grid[r][c] == 0 or (r, c) in visited:
-                return 0
+        def bfs(r, c):
+            queue = deque()
+            queue.append((r, c))
             visited.add((r, c))
             size = 1
-            size += dfs(r + 1, c)
-            size += dfs(r, c + 1)
-            size += dfs(r - 1, c)
-            size += dfs(r, c - 1)
-            return size
 
+            while queue:
+                row, col = queue.popleft()
+                for dr, dc in directions:
+                    new_row, new_col = row + dr, col + dc
+                    if new_row in range(rows) and new_col in range(cols) and (new_row, new_col) not in visited and grid[new_row][new_col] == 1:
+                        queue.append((new_row, new_col))
+                        visited.add((new_row, new_col))
+                        size += 1
+            
+            return size
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and (r, c) not in visited:
-                    size = dfs(r, c)
-                    max_size = max(size, max_size)
+                if (r, c) not in visited and grid[r][c] == 1:
+                    size = bfs(r, c)
+                    max_size = max(max_size, size)
         
         return max_size
