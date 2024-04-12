2a397c38598b576fa7f9bd1574952c0f335c2787
Danylo Sokol <dansokol677@gmail.com>
2024-04-12 11:55:38 +0100

    Time: 625 ms (9.07%) | Memory: 18.4 MB (9.33%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return -1
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        if grid[0][0] == 1 or grid[rows-1][cols-1] == 1:
+            return -1
+
+        def bfs(r, c):
+            visited = set()
+            queue = deque()
+
+            queue.append((r, c))
+            length = 1
+            
+            while queue:
+                for i in range(len(queue)):
+                    row, col = queue.popleft()
+                    if row == rows - 1 and col == cols - 1 and grid[row][col] == 0:
+                        return length
+                    directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+                    for dr, dc in directions:
+                        new_row, new_col = row + dr, col + dc
+                        if new_row in range(rows) and new_col in range(cols) and (new_row, new_col) not in visited and grid[new_row][new_col] == 0:
+                            queue.append((new_row, new_col))
+                            visited.add((new_row, new_col))
+                length += 1
+
+            return -1
+        
+        return bfs(0, 0)
