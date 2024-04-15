7efeb321ba2ddfaa188a004ab7a34fe5af8b6a93
Danylo Sokol <dansokol677@gmail.com>
2024-04-15 09:51:55 +0100

    Time: 52 ms (47.10%) | Memory: 16.7 MB (9.16%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,32 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return -1
         queue = deque()
+        visited = set()
         fresh = 0
         time = 0
         rows, cols = len(grid), len(grid[0])
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
+                if grid[r][c] == 2:
                     queue.append((r, c))
+                    visited.add((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
         
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         while queue and fresh > 0:
             for i in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in directions:
                     new_row, new_col = r + dr, c + dc
-                    if (new_row not in range(rows) or 
-                        new_col not in range(cols) or 
-                        grid[new_row][new_col] != 1):
+                    if (
+                        new_row not in range(rows) or
+                        new_col not in range(cols) or
+                        grid[new_row][new_col] != 1 or
+                        (new_row, new_col) in visited
+                    ):
                         continue
-                    grid[new_row][new_col] = 2
                     queue.append((new_row, new_col))
+                    visited.add((new_row, new_col))
                     fresh -= 1
             time += 1
         
         return time if fresh == 0 else -1
-
+            
