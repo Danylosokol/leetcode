24d75b1114a9b320a098653e5a8cc75d69af92f8
Danylo Sokol <dansokol677@gmail.com>
2024-04-15 10:26:36 +0100

    Time: 438 ms (60.75%) | Memory: 17.7 MB (61.20%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,34 +1,23 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return -1
+        n = len(grid)
+        queue = deque([(0, 0, 1)])
+        visited = set((0, 0))
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
 
-        rows, cols = len(grid), len(grid[0])
+        while queue:
+            row, col, length = queue.popleft()
 
-        if grid[0][0] == 1 or grid[rows-1][cols-1]:
-            return -1
-
-        def bfs(r, c):
-            queue = deque()
-            visited = set()
-            queue.append((r, c))
-            visited.add((r, c))
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
-            length = 1
-
-            while queue:
-                for i in range(len(queue)):
-                    row, col = queue.popleft()
-                    if row == rows - 1 and col == cols - 1:
-                        return length
-                        
-                    for dr, dc in directions:
-                        new_row, new_col = row + dr, col + dc
-                        if new_row in range(rows) and new_col in range(cols) and (new_row, new_col) not in visited and grid[new_row][new_col] == 0:
-                            queue.append((new_row, new_col))
-                            visited.add((new_row, new_col))
-                length += 1
+            if min(row, col) < 0 or max(row, col) >= n or grid[row][col] == 1:
+                continue
             
-            return -1
+            if row == n-1 and col == n-1:
+                return length
+            
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+                if (new_row, new_col) not in visited:
+                    queue.append((new_row, new_col, length + 1))
+                    visited.add((new_row, new_col))
         
-        return bfs(0, 0)
+        return -1 
