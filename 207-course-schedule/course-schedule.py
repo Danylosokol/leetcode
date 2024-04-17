25f6a24f48542974efcac452e4635fc124a0e896
Danylo Sokol <dansokol677@gmail.com>
2024-04-17 18:05:23 +0100

    Time: 146 ms (6.54%) | Memory: 18.6 MB (11.91%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,43 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {}
+        
+        takenCourses = 0
+
+        def dfs_check(parent, child, graph, visited):
+            visited.append(parent)
+            print("checking:")
+            print(child)
+            print("in")
+            print(parent)
+            print(graph[parent])
+            if child in graph[parent]:
+                return True
+            result = False
+            for node in graph[parent]:
+                if node in visited:
+                    continue
+                result = dfs_check(node, child, graph, visited)
+                if result:
+                    return result
+            
+            return result
+
+        for node in prerequisites:
+            if node[0] not in graph:
+                graph[node[0]] = []
+                takenCourses += 1
+                if takenCourses > numCourses:
+                    return False
+            if node[1] not in graph:
+                graph[node[1]] = []
+                takenCourses += 1
+                if takenCourses > numCourses:
+                    return False
+            if node[0] == node[1] or dfs_check(node[1], node[0], graph, []):
+                return False
+            graph[node[0]].append(node[1])
+        print(graph)
+        print(takenCourses)
+        return True
+    
