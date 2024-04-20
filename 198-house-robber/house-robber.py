9adaecb8be453ac778a5c2f4f051923df1056b30
Danylo Sokol <dansokol677@gmail.com>
2024-04-20 12:25:30 +0100

    Time: 39 ms (33.79%) | Memory: 16.4 MB (99.39%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,8 +3,10 @@ class Solution:
         if not nums: return 0
 
         memo = [0 for _ in range(len(nums) + 1)]
+        memo[0] = 0
+        memo[1] = nums[0]
 
-        for i in range(len(nums)):
+        for i in range(1, len(nums)):
             val = nums[i]
             memo[i + 1] = max(memo[i], memo[i - 1] + val)
         
