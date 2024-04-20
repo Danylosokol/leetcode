cdb9e2495614f30b510e33ef44e6575d0e08a88b
Danylo Sokol <dansokol677@gmail.com>
2024-04-20 12:12:31 +0100

    Time: 45 ms (5.30%) | Memory: 16.5 MB (89.81%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,18 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
+        memo = [-1 for _ in range(len(nums))]
 
-        for n in nums:
-            print("n:--------")
-            print(n)
-            temp = max(rob1 + n, rob2)
-            print("temp:")
-            print(temp)
-            print("rob1:")
-            print(rob1)
-            print("rob2:")
-            print(rob2)
-            rob1 = rob2
-            rob2 = temp
+        def rob_rec(nums, idx):
+            if idx < 0:
+                return 0
+            
+            if memo[idx] >= 0:
+                return memo[idx]
+            
+            result = max(rob_rec(nums, idx -2) + nums[idx], rob_rec(nums, idx-1))
+            memo[idx] = result
+            return result
         
-        return rob2
+        return rob_rec(nums, len(nums) -1)
