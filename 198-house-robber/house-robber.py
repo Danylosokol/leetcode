79de5727ba5df6e66c9bfcab63fec03bba98eb94
Danylo Sokol <dansokol677@gmail.com>
2024-04-21 11:45:22 +0100

    Time: 44 ms (7.71%) | Memory: 16.5 MB (89.77%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,11 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums: return 0
-
+        # house before previous
         prev1 = 0
+        # previsous house
         prev2 = 0
 
         for num in nums:
+            # compute max that we can rob up until current house
             temp = max(prev1 + num, prev2)
             prev1 = prev2
             prev2 = temp
