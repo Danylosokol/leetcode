e6264b128cb5710eed4c88516898979fbd2811d2
Danylo Sokol <dansokol677@gmail.com>
2024-04-27 12:35:02 +0100

    Time: 354 ms (56.05%) | Memory: 20.2 MB (94.29%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
@@ -1,41 +1,20 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        max_sub = 1
-        curr_sub = 1
+        l, r = 0, 1
+        res, prev = 1, ""
 
-        is_last_more = False
-        is_last_less = False
-        last_val = arr[0]
-
-        for num in arr[1:]:
-            if not is_last_more and not is_last_less:
-                if last_val > num:
-                    is_last_more = True
-                    is_last_less = False
-                    curr_sub += 1
-                elif last_val < num:
-                    is_last_less = True
-                    is_last_more = False
-                    curr_sub += 1
-                else:
-                    curr_sub = 1
-            elif is_last_more and last_val < num:
-                is_last_less = True
-                is_last_more = False
-                curr_sub += 1
-            elif is_last_less and last_val > num:
-                is_last_more = True
-                is_last_less = False
-                curr_sub += 1
+        while r < len(arr):
+            if arr[r - 1] > arr[r] and prev != ">":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = ">"
+            elif arr[r - 1] < arr[r] and prev != "<":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = "<"
             else:
-                is_last_more = last_val > num
-                is_last_less = last_val < num
-                if last_val != num:
-                    curr_sub = 2
-                else:
-                    curr_sub = 1
-            last_val = num
-            max_sub = max(max_sub, curr_sub)
-
-        return max_sub            
-            
+                r = r + 1 if arr[r] == arr[r - 1] else r
+                l = r - 1
+                prev = ""
+        return res
+        
