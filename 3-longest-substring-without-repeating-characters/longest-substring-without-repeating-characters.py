156deb2c6023e06d41421fb386f2f057ba221df8
Danylo Sokol <dansokol677@gmail.com>
2024-04-30 18:44:57 +0100

    Time: 57 ms (51.39%) | Memory: 16.7 MB (60.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,15 +7,9 @@ class Solution:
         L = 0
 
         for R in range(1, len(s)):
-            print("s[R]:")
-            print(s[R])
             while s[R] in charsMet and R > L:
-                print("in while:")
-                print(charsMet)
                 charsMet.remove(s[L])
                 L += 1
-                print("L now:")
-                print(s[L])
             charsMet.add(s[R])
             maxLength = max(maxLength, R - L + 1)
         
