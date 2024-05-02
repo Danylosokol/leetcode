34e2369adb030601bf15a8b4c7b6e71050761fd4
Danylo Sokol <dansokol677@gmail.com>
2024-05-02 19:22:58 +0100

    Time: 51 ms (25.73%) | Memory: 22.8 MB (5.20%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L, R = 0, 0
+        s = list(s)
+        for R in range(len(s)):
+            if s[R].isalpha() or s[R].isdigit():
+                s[L] = s[R].lower()
+                L += 1
+        s = s[:L]
+        print("sanitised string:")
+        print(s)
+        L, R = 0, len(s) - 1
+        while L <= R:
+            if s[L] != s[R]:
+                return False
+            else:
+                L += 1
+                R -= 1
+
+        return True
