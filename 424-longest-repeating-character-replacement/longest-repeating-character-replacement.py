cdb890d92542af7704a2a9ed64182b0c5ae30fb5
Danylo Sokol <dansokol677@gmail.com>
2024-05-03 19:31:53 +0100

    Time: 330 ms (5.02%) | Memory: 16.8 MB (13.26%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,11 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        result = 0
         L = 0
+        result = 0
 
         for R in range(len(s)):
             count[s[R]] = 1 + count.get(s[R], 0)
+            print("count is")
+            print(count)
             while R - L + 1 - max(count.values()) > k:
                 count[s[L]] -= 1
                 L += 1
