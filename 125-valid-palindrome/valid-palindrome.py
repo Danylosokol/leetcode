d3931f16ed379110b4f4e561c2b167e6a9bf600c
Danylo Sokol <dansokol677@gmail.com>
2024-05-04 12:06:13 +0100

    Time: 58 ms (10.43%) | Memory: 16.9 MB (91.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,18 +4,16 @@ class Solution:
         while L <= R:
             while L < R and not self.isAlphaNumeric(s[L]):
                 L += 1
-            
             while R > L and not self.isAlphaNumeric(s[R]):
                 R -= 1
-            
             if s[L].lower() != s[R].lower():
                 return False
             L += 1
             R -= 1
         return True
     
-    def isAlphaNumeric(self, c:str) -> bool:
-        return (
+    def isAlphaNumeric(self, c):
+        return(
             ord(c) >= ord('0') and ord(c) <= ord('9') or
             ord(c) >= ord('a') and ord(c) <= ord('z') or
             ord(c) >= ord('A') and ord(c) <= ord('Z')
