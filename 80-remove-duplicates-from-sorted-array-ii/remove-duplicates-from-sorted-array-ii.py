ad8e14ea14580d8b24d6d498df966b71e2e5ee0e
Danylo Sokol <dansokol677@gmail.com>
2024-05-05 12:00:15 +0100

    Time: 51 ms (61.31%) | Memory: 16.5 MB (76.65%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -7,7 +7,6 @@ class Solution:
             while R + 1 < len(nums) and nums[R] == nums[R + 1]:
                 R += 1
                 count += 1
-            print("count for: " + str(nums[R]) + " is: " + str(count))
             count = min(2, count)
 
             for _ in range(count):
