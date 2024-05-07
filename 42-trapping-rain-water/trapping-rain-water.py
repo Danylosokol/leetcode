f298aa1bde96df4c2a33e80d21a5ee5abad79a88
Danylo Sokol <dansokol677@gmail.com>
2024-05-07 19:08:09 +0100

    Time: 104 ms (51.89%) | Memory: 18.7 MB (16.68%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,15 +3,16 @@ class Solution:
         L, R = 0, len(height) - 1
         maxL, maxR = height[L], height[R]
         result = 0
-
         while L < R:
             if height[L] <= height[R]:
                 L += 1
                 maxL = max(maxL, height[L])
-                result += maxL - height[L]
+                amount = maxL - height[L]
+                result += amount
             elif height[R] < height[L]:
                 R -= 1
                 maxR = max(maxR, height[R])
-                result += maxR - height[R]
+                amount = maxR - height[R]
+                result += amount
         
         return result
