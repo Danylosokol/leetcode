0af5d9ec8396a0d03e5dc8f7727fb1d7ed1828ca
Danylo Sokol <dansokol677@gmail.com>
2024-05-08 08:44:37 +0100

    Time: 234 ms (15.65%) | Memory: 18 MB (11.73%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefix = []
+        reversedPrefix = []  
+        curr = 0
+        for num in nums:
+            curr += num
+            prefix.append(curr)
+        
+        curr = 0
+        for num in reversed(nums):
+            curr += num
+            reversedPrefix.insert(0, curr)
+
+        print(prefix)
+        print(reversedPrefix)
+        for i in range(len(nums)):
+            if prefix[i] == reversedPrefix[i]:
+                return i
+        
+        return -1
