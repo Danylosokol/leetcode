bd7387e1f009b73038e5c088445da174b4cff58b
Danylo Sokol <dansokol677@gmail.com>
2024-05-18 11:40:48 +0100

    Time: 3965 ms (14.42%) | Memory: 189.5 MB (7.33%) - LeetSync

---
+++ 746-prefix-and-suffix-search/prefix-and-suffix-search.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.weight = 0
+
+    def addWord(self, word, weight):
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr.weight = weight
+            curr = curr.children[c]
+
+class WordFilter:
+
+    def __init__(self, words: List[str]):
+        self.root = TrieNode()
+        for idx, word in enumerate(words):
+            word = word + "#"
+            length = len(word)
+            word += word
+            for i in range(length):
+                self.root.addWord(word[i:], idx)
+
+    def f(self, pref: str, suff: str) -> int:
+        curr = self.root
+        for c in suff + '#' + pref:
+            if c not in curr.children:
+                return -1
+            curr = curr.children[c]
+        
+        return curr.weight
+
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter(words)
+# param_1 = obj.f(pref,suff)
