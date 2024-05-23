86b38d1e2b64c4bb13187bd06ae8b6fb8cc0fb52
Danylo Sokol <dansokol677@gmail.com>
2024-05-23 08:21:09 +0100

    Time: 349 ms (67.86%) | Memory: 31.7 MB (80.66%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,52 +1,15 @@
-class UnionFind:
-    def __init__(self, n):
-        self.par = {}
-        self.rank = {}
-        for i in range(n):
-            self.par[i] = i
-            self.rank[i] = 1
-    
-    def find(self, n):
-        p = self.par[n]
-        while p != self.par[p]:
-            self.par[p] = self.par[self.par[p]]
-            p = self.par[p]
-        
-        return p
-    
-    def union(self, x, y):
-        px, py = self.find(x), self.find(y)
-
-        if px == py:
-            return 
-        
-        if self.rank[px] > self.rank[py]:
-            self.par[py] = px
-            self.rank[px] += self.rank[py]
-        else:
-            self.par[px] = py
-            self.rank[py] += self.rank[px]
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        union = UnionFind(len(nums))
-        numbers = {}
-        for i, num in enumerate(nums):
-            numbers[num] = i
+        numbers = set(nums)
+        longest =  0 
 
-        for i, num in enumerate(nums):
-            if num + 1 in numbers:
-                union.union(i, numbers[num + 1])
-            elif num - 1 in numbers:
-                union.union(i, numbers[num - 1])
+        for num in numbers:
+            if num - 1 not in numbers:
+                length = 1
+
+                while num + length in numbers:
+                    length += 1
+                    
+                longest = max(longest, length)
         
-        mergedNums = defaultdict(list)
-        for num, i in numbers.items():
-            leader = union.find(i)
-            mergedNums[leader].append(num)
-            
-        maxLength = 0
-        for merge in mergedNums.values():
-            maxLength = max(maxLength, len(merge))
-        
-        return maxLength
+        return longest
