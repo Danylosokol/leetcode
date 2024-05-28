410c6a4d9b8de6b1aa626b9e10d21439228a5e35
Danylo Sokol <dansokol677@gmail.com>
2024-05-28 19:27:12 +0100

    Time: 3288 ms (18.75%) | Memory: 57.3 MB (10.00%) - LeetSync

---
+++ 2526-longest-increasing-subsequence-ii/longest-increasing-subsequence-ii.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, total, L, R):
+        self.sum = total
+        self.left = None
+        self.right = None
+        self.L = L
+        self.R = R
+    
+class SegmentTree:
+    def __init__(self, nums):
+        self.root = self.build(nums, 0, len(nums) - 1)
+    
+    def build(self, nums, L, R):
+        if L == R:
+            return Node(0, L, R)
+        root = Node(0, L, R)
+        M = (L + R) // 2
+        root.left = self.build(nums, L, M)
+        root.right = self.build(nums, M + 1, R)
+        return root
+    
+    def update(self, index, val):
+        self.update_helper(self.root, index, val)
+    
+    def update_helper(self, root, index, val):
+        if root.L == root.R:
+            root.sum = val
+            return
+        M = (root.L + root.R) // 2
+        if index > M:
+            self.update_helper(root.right, index, val)
+        else:
+            self.update_helper(root.left, index, val)
+        root.sum = max(root.left.sum, root.right.sum)
+    
+    def query(self, L, R):
+        return self.query_helper(self.root, L, R)
+    
+    def query_helper(self, root, L, R):
+        if root.L >= L and root.R <= R:
+            return root.sum
+        M = (root.L + root.R) // 2
+        max_value = 0
+        if L <= M:
+            max_value = self.query_helper(root.left, L, R)
+        if R > M:
+            max_value = max(max_value, self.query_helper(root.right, L, R))
+        return max_value
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int], k: int) -> int:
+        tree = SegmentTree([i for i in range(max(nums) + 1)])
+        max_length = 1
+        for value in nums:
+            length = tree.query(value - k, value - 1) + 1
+            print(length)
+            max_length = max(max_length, length)
+            tree.update(value, length)
+        return max_length
