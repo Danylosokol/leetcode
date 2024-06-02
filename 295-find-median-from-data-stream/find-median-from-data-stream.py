460a42de6da1821e6d1055e84b69f919332bdce5
Danylo Sokol <dansokol677@gmail.com>
2024-06-02 11:37:44 +0100

    Time: 397 ms (31.47%) | Memory: 38.5 MB (43.26%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -15,15 +15,16 @@ class MedianFinder:
             heapq.heappush(self.large, val)
         elif len(self.large) > len(self.small) + 1:
             val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1 * val)
+            heapq.heappush(self.small, val * -1)
 
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return -1 * self.small[0]
         elif len(self.large) > len(self.small):
             return self.large[0]
-        
-        return (-1 * self.small[0] + self.large[0]) / 2
+        else:
+            return (-1 * self.small[0] + self.large[0]) / 2
+
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
