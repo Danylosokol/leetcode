5452806e4796b12798c465ae224a8b9452298521
Danylo Sokol <dansokol677@gmail.com>
2024-06-05 07:44:38 +0100

    Time: 32 ms (92.34%) | Memory: 16.6 MB (84.99%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -3,16 +3,17 @@ class Solution:
         nums.sort()
         subsets = []
         currSubset = []
-        self.helper(0, nums, currSubset, subsets)
-        return subsets
 
-    def helper(self, i, nums, currSubset, subsets):
+        self.helper(0, nums, subsets, currSubset)
+        return subsets
+    
+    def helper(self, i, nums, subsets, currSubset):
         if i >= len(nums):
             subsets.append(currSubset.copy())
             return
         currSubset.append(nums[i])
-        self.helper(i + 1, nums, currSubset, subsets)
+        self.helper(i + 1, nums, subsets, currSubset)
         currSubset.pop()
         while i + 1 < len(nums) and nums[i] == nums[i + 1]:
             i += 1
-        self.helper(i + 1, nums, currSubset, subsets)
+        self.helper(i + 1, nums, subsets, currSubset)
