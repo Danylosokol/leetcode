746dc542a73d30c636aa3b57862b72c0f9d337d3
Danylo Sokol <dansokol677@gmail.com>
2024-06-05 07:58:38 +0100

    Time: 35 ms (81.24%) | Memory: 16.7 MB (44.95%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -4,16 +4,16 @@ class Solution:
         subsets = []
         currSubset = []
 
-        self.helper(0, nums, subsets, currSubset)
+        self.backtrack(0, nums, subsets, currSubset)
         return subsets
     
-    def helper(self, i, nums, subsets, currSubset):
+    def backtrack(self, i, nums, subsets, currSubset):
         if i >= len(nums):
             subsets.append(currSubset.copy())
             return
         currSubset.append(nums[i])
-        self.helper(i + 1, nums, subsets, currSubset)
+        self.backtrack(i + 1, nums, subsets, currSubset)
         currSubset.pop()
         while i + 1 < len(nums) and nums[i] == nums[i + 1]:
             i += 1
-        self.helper(i + 1, nums, subsets, currSubset)
+        self.backtrack(i + 1, nums, subsets, currSubset)
