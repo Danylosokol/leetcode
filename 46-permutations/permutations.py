63744533fbca9756722a23bc2161edfb35797898
Danylo Sokol <dansokol677@gmail.com>
2024-06-08 12:22:52 +0100

    Time: 40 ms (59.64%) | Memory: 16.6 MB (93.71%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return self.backtrack(0, nums)
-    
-    def backtrack(self, i, nums):
-        if i == len(nums):
-            return [[]]
-        
-        result = []
-        perm = self.backtrack(i + 1, nums)
+        perms = [[]]
 
-        for p in perm:
-            for j in range(len(p) + 1):
-                pCopy = p.copy()
-                pCopy.insert(j, nums[i])
-                result.append(pCopy)
+        for num in nums:
+            nextPerm = []
+            for p in perms:
+                for j in range(len(p) + 1):
+                    pCopy = p.copy()
+                    pCopy.insert(j, num)
+                    nextPerm.append(pCopy)
+            perms = nextPerm
         
-        return result
+        return perms
