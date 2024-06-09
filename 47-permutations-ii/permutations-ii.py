3ed4d7ff4e092e63133d978e23996aa7ee176137
Danylo Sokol <dansokol677@gmail.com>
2024-06-09 12:10:00 +0100

    Time: 53 ms (58.71%) | Memory: 16.7 MB (76.53%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,22 +1,22 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        return self.backtrack(0, nums)
-
-    def backtrack(self, i, nums):
-        if i == len(nums):
-            return [[]]
-        
         result = []
-        # while i + 1 < len(nums) and nums[i] == nums[i + 1]:
-        #     i = i + 1
-        perm = self.backtrack(i + 1, nums)
-
-        for p in perm:
-            for j in range(len(p) + 1):
-                pCopy = p.copy()
-                pCopy.insert(j, nums[i])
-                if pCopy not in result:
-                    result.append(pCopy)
+        perms = []
+        counts = {n: 0 for n in nums}
+        for n in nums:
+            counts[n] += 1
         
+        def backtrack():
+            if len(perms) == len(nums):
+                result.append(perms[:])
+            
+            for n in counts:
+                if counts[n] > 0:
+                    perms.append(n)
+                    counts[n] -= 1
+                    backtrack()
+                    perms.pop()
+                    counts[n] += 1
+            
+        backtrack()
         return result
