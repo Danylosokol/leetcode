8e8fdf8816a10823ed46e86b58f95ae3c2aae180
Danylo Sokol <dansokol677@gmail.com>
2024-06-10 08:16:03 +0100

    Time: 383 ms (30.20%) | Memory: 18.5 MB (78.74%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,26 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = {}
+
+        for i in range(1, n + 1):
+            adj[i] = []
+
+        for s, d, w in times:
+            adj[s].append((w, d))
+        
+        minHeap = [(0, k)]
+        shortest = {}
+
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+
+            if n1 in shortest:
+                continue
+            shortest[n1] = w1
+
+            for w2, n2 in adj[n1]:
+                if n2 not in shortest:
+                    heapq.heappush(minHeap, (w1 + w2, n2))
+        result = max(shortest.values())
+        print(shortest)
+        return result if result and len(shortest.keys()) == n else -1
