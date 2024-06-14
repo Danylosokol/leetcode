cc4c03eb84e9f65e38ce304c09ac62e85cd242c3
Danylo Sokol <dansokol677@gmail.com>
2024-06-14 15:54:38 +0100

    Time: 2293 ms (34.67%) | Memory: 188.1 MB (10.49%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -4,30 +4,30 @@ class Solution:
 
         for i in range(len(points)):
             adj[i] = []
-        
+
         for i in range(len(points)):
             for j in range(i + 1, len(points)):
                 distance = abs(points[j][0] - points[i][0]) + abs(points[j][1] - points[i][1])
-                adj[i].append((distance, j))
-                adj[j].append((distance, i))
+                adj[i].append([distance, j])
+                adj[j].append([distance, i])
         
         min_heap = []
         visited = set()
 
-        for distance, neighbour in adj[0]:
-            heapq.heappush(min_heap, [distance, 0, neighbour])
-
+        for d, n in adj[0]:
+            heapq.heappush(min_heap, [d, 0, n])
+        
         visited.add(0)
         result = 0
 
-        while min_heap:
+        while len(visited) < len(points):
             d1, s1, n1 = heapq.heappop(min_heap)
 
             if n1 in visited:
                 continue
-            
-            result += d1
+
             visited.add(n1)
+            result += d1
 
             for d2, n2 in adj[n1]:
                 if n2 not in visited:
@@ -35,3 +35,4 @@ class Solution:
         
         return result
 
+        
