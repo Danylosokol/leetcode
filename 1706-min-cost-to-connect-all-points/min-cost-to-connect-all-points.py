320eb398bf271f21a2a8506d29394fbd33ac4714
Danylo Sokol <dansokol677@gmail.com>
2024-06-16 12:32:27 +0100

    Time: 1690 ms (50.22%) | Memory: 105.9 MB (50.75%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,36 +1,43 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, v):
+        while v != self.par[v]:
+            self.par[v] = self.par[self.par[v]]
+            v = self.par[v]
+        return v
+    
+    def union(self, v1, v2):
+        p1, p2 = self.find(v1), self.find(v2)
+
+        if p1 == p2:
+            return False
+        
+        if p1 > p2:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        adj = {}
+        edges = []
 
-        for i in range(len(points)):
-            adj[i] = []
-        
         for i in range(len(points)):
             for j in range(i + 1, len(points)):
-                distance = abs(points[j][0] - points[i][0]) + abs(points[j][1] - points[i][1])
-                adj[i].append([distance, j])
-                adj[j].append([distance, i])
+                weight = abs(points[j][0] - points[i][0]) + abs(points[j][1] - points[i][1])
+                edges.append([i, j, weight])
         
-        min_heap = []
-        visited = set()
-
-        for d, n in adj[0]:
-            heapq.heappush(min_heap, [d, n])
+        edges.sort(key = lambda e: e[2])
         
-        visited.add(0)
-        result = 0
+        uf = UnionFind(len(points))
+        mst_weight = 0
+        for p1, p2, w in edges:
+            if uf.union(p1, p2):
+                mst_weight += w
 
-        while len(visited) < len(points):
-            d1, n1 = heapq.heappop(min_heap)
-
-            if n1 in visited:
-                continue
-            
-            visited.add(n1)
-            result += d1
-            
-            for d2, n2 in adj[n1]:
-                if n2 not in visited:
-                    heapq.heappush(min_heap, [d2, n2])
-        
-        return result
+        return mst_weight
