31b67942ff1205195bf95e494a6dca88e9a66b5d
Danylo Sokol <dansokol677@gmail.com>
2024-06-19 14:42:11 +0100

    Time: 110 ms (12.27%) | Memory: 18.7 MB (5.64%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,20 +1,21 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = {i:[] for i in range(numCourses)}
-
+        adj = {i: [] for i in range(numCourses)}
+        
         for crs, pre in prerequisites:
             adj[crs].append(pre)
         
-        topSort = []
-        path = set()
+        top_sort = []
         visited = set()
+        path = set()
 
         for crs in adj:
-            if not self.dfs(crs, adj, visited, path, topSort):
+            if not self.dfs(crs, visited, path, adj, top_sort):
                 return []
-        return topSort
+        
+        return top_sort
     
-    def dfs(self, crs, adj, visited, path, topSort):
+    def dfs(self, crs, visited, path, adj, top_sort):
         if crs in path:
             return False
         
@@ -25,9 +26,9 @@ class Solution:
         path.add(crs)
 
         for pre in adj[crs]:
-            if not self.dfs(pre, adj, visited, path, topSort):
+            if not self.dfs(pre, visited, path, adj, top_sort):
                 return False
         
-        topSort.append(crs)
         path.remove(crs)
+        top_sort.append(crs)
         return True
