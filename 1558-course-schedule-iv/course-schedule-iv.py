5a900c5c2154c7336295d95bb4826490296df2be
Danylo Sokol <dansokol677@gmail.com>
2024-06-20 19:06:57 +0100

    Time: 572 ms (70.86%) | Memory: 20.2 MB (68.56%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,25 +1,23 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        adj = {i: [] for i in range(numCourses)}
+        adj = {i:[] for i in range(numCourses)}
 
         for pre, crs in prerequisites:
             adj[crs].append(pre)
         
-        prereqMap = {}
+        preHashmap = {}
 
         def dfs(crs):
-            if crs not in prereqMap:
-                prereqMap[crs] = set()
+            if crs not in preHashmap:
+                preHashmap[crs] = set()
                 for pre in adj[crs]:
-                    prereqMap[crs] |= dfs(pre)
-                prereqMap[crs].add(crs)
-            return prereqMap[crs]
-
+                    preHashmap[crs] |= dfs(pre)
+                preHashmap[crs].add(crs)
+            return preHashmap[crs]
         for crs in adj:
             dfs(crs)
-
         result = []
         for u, v in queries:
-            result.append(u in prereqMap[v])
+            result.append(u in preHashmap[v])
         
         return result
