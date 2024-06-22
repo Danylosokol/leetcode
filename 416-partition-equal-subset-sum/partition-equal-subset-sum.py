a0e514ccd95358e365f73232c23246d215c5bce1
Danylo Sokol <dansokol677@gmail.com>
2024-06-22 12:39:38 +0100

    Time: 538 ms (66.89%) | Memory: 19.3 MB (51.33%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,23 +1,16 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        capacity = sum(nums) / 2
-        if not capacity.is_integer():
+        if sum(nums) % 2:
             return False
-        N, M = len(nums), int(capacity)
-        dp = [0] * (M + 1)
+        
+        target = sum(nums) // 2
+        dp = set()
+        dp.add(0)
 
-        for c in range(M + 1):
-            if nums[0] <= c:
-                dp[c] = nums[0]
- 
-        for i in range(1, N):
-            cur_row = [0] * (M + 1)
-            for c in range(1, M + 1):
-                skip = dp[c]
-                include = 0 
-                if c - nums[i] >= 0:
-                    include = nums[i] + dp[c - nums[i]]
-                cur_row[c] = max(skip, include)
-            dp = cur_row
-
-        return dp[M] == capacity
+        for i in range(len(nums) - 1, -1, -1):
+            next_dp = set()
+            for t in dp:
+                next_dp.add(t + nums[i])
+            dp |= next_dp
+        
+        return target in dp 
