f370920d850c70916ee82bb2de9d85b1017bf0eb
Danylo Sokol <dansokol677@gmail.com>
2024-06-23 11:21:55 +0100

    Time: 2528 ms (7.48%) | Memory: 17.3 MB (76.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,16 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2:
+        if sum(nums)%2:
             return False
         
-        target = sum(nums) // 2
-        df = set()
-        df.add(0)
+        capacity = sum(nums) // 2
+        N, M = len(nums), capacity
+        df = [0] * (M + 1)
 
-        for i in range(len(nums) - 1, -1, -1):
-            next_df = set()
-            for t in df:
-                next_df.add(t + nums[i])
-            df |= next_df
+        for c in range(M + 1):
+            if nums[0] <= c:
+                df[c] = nums[0]
+
+        for i in range(1, N):
+            new_row = [0] * (M + 1)
+            for c in range(1, M + 1):
+                skip = df[c]
+                include = 0
+                if c - nums[i] >= 0:
+                    include = nums[i] + df[c - nums[i]]
+                new_row[c] = max(skip, include)
+            df = new_row
+
+        return df[M] == capacity
         
-        return target in df
