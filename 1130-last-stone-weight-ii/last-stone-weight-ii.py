1768ae77591a137bd5330d26e39e60a69ce34d50
Danylo Sokol <dansokol677@gmail.com>
2024-06-29 16:19:46 +0100

    Time: 67 ms (42.23%) | Memory: 16.6 MB (70.07%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -1,15 +1,14 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        stoneSum = sum(stones)
-        target = ceil(stoneSum / 2)
-
+        stonesSum = sum(stones)
+        target = ceil(stonesSum/2)
         N, M = len(stones), target
         dp = [0] * (M + 1)
 
         for c in range(M + 1):
             if c >= stones[0]:
                 dp[c] = stones[0]
-
+        
         for i in range(1, N):
             newRow = [0] * (M + 1)
             for c in range(1, M + 1):
@@ -19,5 +18,4 @@ class Solution:
                     include = stones[i] + dp[c - stones[i]]
                 newRow[c] = max(skip, include)
             dp = newRow
-
-        return abs(dp[M] - (stoneSum - dp[M]))
+        return abs(dp[M] - (stonesSum - dp[M]))
