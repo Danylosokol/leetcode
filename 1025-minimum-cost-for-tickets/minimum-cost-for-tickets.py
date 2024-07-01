7a2e42d5d7e7579e3ab2d811bdd7a60b65682c54
Danylo Sokol <dansokol677@gmail.com>
2024-07-01 08:18:48 +0100

    Time: 50 ms (31.94%) | Memory: 16.7 MB (53.47%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -10,12 +10,14 @@ class Solution:
                 return dp[i]
             
             dp[i] = float("inf")
+
             for d, c in zip([1, 7, 30], costs):
                 j = i
                 while j < len(days) and days[j] < days[i] + d:
                     j += 1
-                dp[i] = min(dp[i],c + dfs(j))
-
+                
+                dp[i] = min(dp[i], c + dfs(j))
+            
             return dp[i]
         
         return dfs(0)
