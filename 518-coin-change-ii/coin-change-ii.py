747d9566488a2783b27d1eb05107bfed9febaf6c
Danylo Sokol <dansokol677@gmail.com>
2024-07-03 07:46:41 +0100

    Time: 590 ms (18.65%) | Memory: 157.8 MB (19.66%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -2,17 +2,17 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = {}
 
-        def dfs(i, cur_amount):
-            if cur_amount == amount:
+        def dfs(i, a):
+            if a == amount:
                 return 1
             
-            if cur_amount > amount or i >= len(coins):
+            if i >= len(coins) or a > amount:
                 return 0
             
-            if (i, cur_amount) in dp:
-                return dp[(i, cur_amount)]
+            if (i, a) in dp:
+                return dp[(i, a)]
             
-            dp[(i, cur_amount)] = dfs(i, cur_amount + coins[i]) + dfs(i + 1, cur_amount)
-            return dp[(i, cur_amount)]
+            dp[(i, a)] = dfs(i + 1, a) + dfs(i, a + coins[i])
+            return dp[(i, a)]
         
         return dfs(0, 0)
