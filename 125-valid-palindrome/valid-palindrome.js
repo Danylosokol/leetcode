f9a2225934c9af2d56ff6a3fb9318bf45180164c
Danylo Sokol <danylo.sokol2504@gmail.com>
2024-08-24 16:42:53 +0100

    Time: 51 ms (95.34%) | Memory: 56.4 MB (14.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,22 +4,16 @@
  */
 var isPalindrome = function(s) {
     let L = 0, R = s.length - 1
-    console.log(s, L, R)
     while(L <= R){
-        console.log("L: ", L, s[L])
-        console.log("R: ", R, s[R])
         if(!s[L].match(/[a-z0-9]/i)){
             L++
-            console.log("L is not alphanumeric")
             continue
         }
         if(!s[R].match(/[a-z0-9]/i)){
             R--
-            console.log("R is not alphanumeric")
             continue
         }
         if(s[L].toLowerCase() !== s[R].toLowerCase()){
-            console.log("string is not alphanumeric")
             return false
         }
         L++
