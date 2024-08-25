f8b7f1e626265b3c08a34dbd64b09d7c86c43aec
Danylo Sokol <danylo.sokol2504@gmail.com>
2024-08-25 11:49:57 +0100

    Time: 49 ms (78.48%) | Memory: 49.1 MB (49.43%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -5,27 +5,27 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    let indx1 = m - 1, indx2 = n - 1, currIndx = nums1.length - 1;
+var merge = function (nums1, m, nums2, n) {
+    let indx1 = m - 1, indx2 = n - 1, currIndx = nums1.length - 1
 
-    while(indx1 >= 0 && indx2 >= 0){
-        if(nums1[indx1] >= nums2[indx2]){
+    while (indx1 >= 0 && indx2 >= 0) {
+        if (nums1[indx1] >= nums2[indx2]) {
             nums1[currIndx] = nums1[indx1]
             indx1--
-        }else{
+        } else {
             nums1[currIndx] = nums2[indx2]
             indx2--
         }
         currIndx--
     }
 
-    while(indx1 >= 0 && currIndx >= 0){
+    while (indx1 >= 0 && currIndx >= 0) {
         nums1[currIndx] = nums1[indx1]
         indx1--
         currIndx--
     }
 
-    while(indx2 >= 0 && currIndx >= 0){
+    while (indx2 >= 0 && currIndx >= 0) {
         nums1[currIndx] = nums2[indx2]
         indx2--
         currIndx--
