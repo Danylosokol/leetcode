34db02f97747f62a860bc55810b0cc2716397846
Danylo Sokol <danylo.sokol2504@gmail.com>
2024-11-16 19:42:24 +0000

    Time: 16 ms (32.93%) | Memory: 56.7 MB (76.73%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -2,11 +2,7 @@ public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         Array.Sort(nums);
         for(int i = 1; i < nums.Length; i ++){
-            Console.WriteLine(nums[i]);
             if(nums[i] == nums[i - 1]){
-                Console.WriteLine("duplicates:");
-                Console.WriteLine(nums[i]);
-                Console.WriteLine(nums[i - 1]);
                 return true;
             }
         }
